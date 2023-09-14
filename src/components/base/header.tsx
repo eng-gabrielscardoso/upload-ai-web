f8837032911cef6fa3b175488f5e4d6abcb6a8f5
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-09-14 12:55:13 -0300

    fix: typo

--- src/components/base/header.tsx
+++ src/components/base/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="px-6 py-3 flex items-center justify-between border-b">
       <h1 className="text-xl font-bold">upload.ai</h1>
       <div className="flex items-center gap-3">
-        <span className="text-sm text-muted-foreground">Desenvolvido com 💚 by <a href="http://eng-gabrielscardoso.github.io" target="_blank" rel="noopener noreferrer">Gabriel Santos Cardoso</a></span>
+        <span className="text-sm text-muted-foreground">Develop with 💚 by <a href="http://eng-gabrielscardoso.github.io" target="_blank" rel="noopener noreferrer">Gabriel Santos Cardoso</a></span>
         <Separator orientation="vertical" className="h-6" />
         <Button variant="outline">
           <a className="flex items-center" href="http://github.com/eng-gabrielscardoso/upload-ai-web" target="_blank" rel="noopener noreferrer">
